d63773770dce0fe9d080e2b4d311582e81992053
RubenOr <rubenorozco776@gmail.com>
2021-11-18 22:21:33 -0600

    members list

--- stinki/bot.py
+++ stinki/bot.py
@@ -14,26 +14,28 @@ bot = commands.Bot(command_prefix="!",intents=intents)
 
 @bot.event
 async def on_ready():
-    
     print(f'{bot.user} has connected to {bot.guilds[0].name}!')
     channel = bot.get_channel(910630657716260877)
+
     time = datetime.now().strftime('%H:%M')
-    print(time)
-    init = 'stinki started at %sCST' % time 
+    init = 'stinki started at %sCST' % time
+
     await channel.send(init)
     #channel = discord.TextChannel
         
 @bot.command()
 async def whostinki(ctx):
-    for member in ctx.guild.members:
+    members = ctx.guild.members
+    for member in members:
         print('%s' % member.name)
-    #print(members)
-    msg = "ur stinki"
-    await ctx.send(msg)
     
-@bot.command()
-async def test(ctx):
-    print(ctx.guild.users)
-    #await ctx.send('test')
+    ran = random.randint(0, ctx.guild.member_count-1)
+    #print(members)
+    stinki = members[ran]
+    
+    msg = f'{stinki.mention} is stinki'
+    await ctx.send(msg)
+
+
 
 bot.run(TOKEN)
